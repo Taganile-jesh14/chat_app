c4c3d50b88d243b3c625cd0fecb9efca2544e81a
Jesh Taganile <taganilejesh@gmail.com>
2025-05-27 19:16:18 +0800

    Initial commit with chat_bubble and chat_input

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,99 +1,40 @@
+import 'package:chat_app/widgets/chat_bubble.dart';
+import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
 
-  Widget messageBubble(String text, String imageUrl) {
-    return Container(
-      padding: EdgeInsets.all(16),
-      margin: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-      decoration: BoxDecoration(
-        color: Colors.grey[800],
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(12),
-          topRight: Radius.circular(12),
-          bottomLeft: Radius.circular(12),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            text,
-            style: TextStyle(fontSize: 16, color: Colors.white),
-          ),
-          SizedBox(height: 10),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(8),
-            child: Image.network(imageUrl, height: 200, fit: BoxFit.cover),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black, // background for contrast
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text('Hi Pooja!'),
+        title: Text('Hi Divine!'),
         actions: [
           IconButton(
-            onPressed: () {
-              print('Logout pressed!');
-            },
-            icon: Icon(Icons.logout),
-          ),
+              onPressed: () {
+                print('Icon pressed!');
+              },
+              icon: Icon(Icons.logout))
         ],
       ),
       body: Column(
         children: [
           Expanded(
             child: ListView(
-              padding: EdgeInsets.only(top: 10),
               children: [
-                messageBubble(
-                  'Hi, this is your message!',
-                  'https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG',
-                ),
-                messageBubble(
-                  'Another cute panda!',
-                  'https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG',
-                ),
-                messageBubble(
-                  'Still love pandas?',
-                  'https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG',
-                ),
+                ChatBubble(
+                    alignment: Alignment.centerLeft,
+                    message: "Hello, this is Divine"),
+                ChatBubble(
+                    alignment: Alignment.centerRight,
+                    message: "Hi!!"),
               ],
             ),
           ),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-            decoration: BoxDecoration(
-              color: Colors.grey[900],
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-            ),
-            child: Row(
-              children: [
-                Icon(Icons.add, color: Colors.white),
-                SizedBox(width: 10),
-                Expanded(
-                  child: TextField(
-                    style: TextStyle(color: Colors.white),
-                    decoration: InputDecoration(
-                      hintText: 'Type a message...',
-                      hintStyle: TextStyle(color: Colors.white54),
-                      border: InputBorder.none,
-                    ),
-                  ),
-                ),
-                Icon(Icons.send, color: Colors.white),
-              ],
-            ),
-          ),
+          ChatInput(),
         ],
       ),
     );
