7effe74344f6d4588d4f0d91e5cf15eb61390d83
Jesh Taganile <taganilejesh@gmail.com>
2025-05-29 01:04:58 +0800

    Learning about buttons

--- lib/counter_stateful_demo.dart
+++ lib/counter_stateful_demo.dart
@@ -7,32 +7,42 @@ class CounterStateful extends StatefulWidget {
   CounterStateful ({Key? key, required this.buttonColor}) : super(key: key);
 
   @override
-  State<CounterStateful>  createState() {
-    State<CounterStateful> stateClassAssociatedWithThisWidget =
-    _CounterStatefulState();
-    return stateClassAssociatedWithThisWidget;
-  }
+  State<CounterStateful>  createState() => _CounterStatefulState();
 }
 
 class _CounterStatefulState extends State<CounterStateful> {
   int counter = 0;
 
   void increment(){
-    setState(() {
-      counter++;
-    });
+    if(mounted) {
+      setState(() {
+        counter++;
+      });
+    }
     print(counter);
+  }
+
+  @override
+  void initState(){
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose(){
+    // TODO: implement dispose
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Counter'),
+        title: Text('Counter!!!'),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: widget.buttonColor,
-        child: Icon(Icons.add),
+        child: Icon(Icons.message),
         onPressed: () {
           increment();
         },
