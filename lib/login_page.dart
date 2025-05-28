0da40486069f12b1f21b04aeebdf391cc5bb7439
Jesh Taganile <taganilejesh@gmail.com>
2025-05-28 21:11:35 +0800

    Building Basic Widget for the Login Screen

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,32 +6,48 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text(
-              'Let\'s sign you in!',
-              style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 0.5
-              ),
+      appBar: AppBar(),
+      drawer: Drawer(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Button clicked');
+        },
+      ),
+      body: Column(
+        children: [
+          Text(
+            'Let\'s sign you in!',
+            style: TextStyle(
+                fontSize: 30,
+                color: Colors.brown,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.5
             ),
-            Text(
-              'Welcome back! \n You\'ve been missed!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blueGrey
-              ),
+          ),
+          Text(
+            'Welcome back! \n You\'ve been missed!',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, fontSize: 20, color: Colors.blueGrey
             ),
-            Image.network(
-                'https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG', height: 200
+          ),
+          Image.network('https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG', height: 200),
+          Container(
+            height: 150,
+            width: 150,
+            // child: FlutterLogo(),
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(100),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(
+                      'https://upload.wikimedia.org/wikipedia/commons/0/0f/Grosser_Panda.JPG'),
+                ),
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(24)
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
