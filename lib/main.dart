c1cc09985f2699d02149141788c9a049ddc0a0ec
Jesh Taganile <taganilejesh@gmail.com>
2025-05-29 16:09:24 +0800

    Static initializers

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 
-
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await AuthService.init();
   runApp(Provider(
     create: (BuildContext context) => AuthService(),
     child: ChatApp(),
