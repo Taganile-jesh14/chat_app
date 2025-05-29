0c0b997fca191b3a3c78201849f7fb329ce8ffc1
Jesh Taganile <taganilejesh@gmail.com>
2025-05-29 16:35:16 +0800

    Creating a dynamic home page

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -69,8 +69,8 @@ class _ChatPageState extends State<ChatPage> {
           onPressed: () {
             context.read<AuthService>().updateUserName("New Name!");
     },
-          icon: Icon(Icons.logout)),
-          
+              icon: Icon(Icons.change_circle)),
+
         ],
       ),
       body: Column(
