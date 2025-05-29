c7de45b00cace774716af724c868cbcef39e4db0
Jesh Taganile <taganilejesh@gmail.com>
2025-05-29 14:20:59 +0800

    FutureBuilder for Futures

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -42,7 +42,7 @@ class _ChatPageState extends State<ChatPage> {
     });
   }
 
-  _getNetworkImages() async {
+  Future<List<PixelfordImage>> _getNetworkImages() async {
     var endpointUrl = Uri.parse('https://pixelford.com/api2/images');
 
     final response = await http.get(endpointUrl);
@@ -55,6 +55,9 @@ class _ChatPageState extends State<ChatPage> {
       }).toList();
 
       print(_imageList[0].urlFullSize);
+      return _imageList;
+    }else {
+      throw Exception('API not successful!');
     }
   }
 
@@ -86,6 +89,13 @@ class _ChatPageState extends State<ChatPage> {
       ),
       body: Column(
         children: [
+          FutureBuilder<List<PixelfordImage>>(
+              future: _getNetworkImages(),
+              builder: (BuildContext context, AsyncSnapshot<List<PixelfordImage>> snapshot){
+                if(snapshot.hasData) return Image.network(snapshot.data![0].urlSmallSize);
+
+                return CircularProgressIndicator();
+              }),
           Expanded(
             child: ListView.builder(
                 itemCount: _messages.length,
