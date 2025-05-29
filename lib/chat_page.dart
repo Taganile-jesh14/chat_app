508131029fc9ed0ffef9521d4551808b46611622
Jesh Taganile <taganilejesh@gmail.com>
2025-05-29 12:17:22 +0800

    Generics in Dart

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -4,7 +4,29 @@ import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({Key? key}) : super(key: key);
+  ChatPage({Key? key}) : super(key: key);
+
+  List<ChatMessageEntity> _messages = [
+    ChatMessageEntity(
+        author: Author(userName: 'Jesh'),
+        createdAt: 21452315,
+        id: '1',
+        text: 'Wazzup!'
+    ),
+    ChatMessageEntity(
+      author: Author(userName: 'Jesh'),
+      createdAt: 21452315,
+      id: '1',
+      text: 'Hey yow!',
+      imageUrl:  'https://cdn.pixabay.com/photo/2015/10/31/17/33/press-1015988_1280.jpg',
+    ),
+    ChatMessageEntity(
+      author: Author(userName: 'Josh'),
+      createdAt: 21452315,
+      id: '1',
+      text: 'Gottch',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -30,20 +52,13 @@ class ChatPage extends StatelessWidget {
         children: [
           Expanded(
             child: ListView.builder(
-                itemCount: 10,
+                itemCount: _messages.length,
                 itemBuilder: (context, index){
                   return ChatBubble(
-                    alignment: index % 2 == 0
-                        ? Alignment.centerLeft
-                        : Alignment.centerRight,
-
-                    entity: ChatMessageEntity(
-                        id: '1234',
-                        text: 'Hello this is Jesh!',
-                        createdAt: DateTime.now().millisecondsSinceEpoch,
-                        author: Author(userName: 'Jesh123')
-                    ),
-                  );
+                      alignment: _messages[index].author.userName == 'Jesh'
+                          ? Alignment.centerRight
+                          : Alignment.centerLeft,
+                      entity: _messages[index]);
                 }
             ),
           ),
